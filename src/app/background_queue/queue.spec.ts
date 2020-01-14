ebdb7ea7e874f6a38581095b23b60bd94c4f0f96
Gus Narea <gnarea@users.noreply.github.com>
2020-01-14 22:25:55 +0000

    Require private key to get private key

--- src/app/background_queue/queue.spec.ts
+++ src/app/background_queue/queue.spec.ts
@@ -3,18 +3,25 @@ import { initQueue } from './queue';
 
 describe('initQueue', () => {
   const stubRedisHost = 'redis';
+  const stubEndpointPrivateKey = 'secret';
+  const stubEnvVars = { ENDPOINT_PRIVATE_KEY: stubEndpointPrivateKey, REDIS_HOST: stubRedisHost };
 
   afterAll(() => {
     jest.restoreAllMocks();
   });
 
+  test('Error should be thrown if ENDPOINT_PRIVATE_KEY is absent', () => {
+    mockEnvVars({ ...stubEnvVars, ENDPOINT_PRIVATE_KEY: undefined });
+    expect(initQueue).toThrowWithMessage(Error, /ENDPOINT_PRIVATE_KEY/);
+  });
+
   test('Error should be thrown if REDIS_HOST is undefined', () => {
-    mockEnvVars({});
+    mockEnvVars({ ...stubEnvVars, REDIS_HOST: undefined });
     expect(initQueue).toThrowWithMessage(Error, /REDIS_HOST/);
   });
 
   test('REDIS_HOST variable should be used by queue', () => {
-    mockEnvVars({ REDIS_HOST: stubRedisHost });
+    mockEnvVars(stubEnvVars);
 
     const queue = initQueue();
 
@@ -22,7 +29,7 @@ describe('initQueue', () => {
   });
 
   test('Redis port should default to 6379', () => {
-    mockEnvVars({ REDIS_HOST: stubRedisHost });
+    mockEnvVars(stubEnvVars);
 
     const queue = initQueue();
 
@@ -31,7 +38,7 @@ describe('initQueue', () => {
 
   test('REDIS_PORT should be used as Redis port if set', () => {
     const stubPort = 1234;
-    mockEnvVars({ REDIS_HOST: stubRedisHost, REDIS_PORT: stubPort.toString() });
+    mockEnvVars({ ...stubEnvVars, REDIS_PORT: stubPort.toString() });
 
     const queue = initQueue();
 
@@ -39,7 +46,7 @@ describe('initQueue', () => {
   });
 
   test('Queue name should be set to "pong"', () => {
-    mockEnvVars({ REDIS_HOST: stubRedisHost });
+    mockEnvVars(stubEnvVars);
 
     const queue = initQueue();
 
