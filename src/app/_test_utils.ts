6b2599ecb6d459c75e0ca2ef8c42618e8e0117bc
Gus Narea <gnarea@users.noreply.github.com>
2020-01-15 12:31:10 +0000

    Implement ping serialization

--- src/app/_test_utils.ts
+++ src/app/_test_utils.ts
@@ -1,3 +1,4 @@
+import { Certificate, issueNodeCertificate } from '@relaycorp/relaynet-core';
 import envVar from 'env-var';
 
 export function getMockContext(mockedObject: any): jest.MockContext<any, any> {
@@ -5,11 +6,43 @@ export function getMockContext(mockedObject: any): jest.MockContext<any, any> {
   return mockInstance.mock;
 }
 
-export function mockEnvVars(envVars: { readonly [key: string]: string }): void {
+export function mockEnvVars(envVars: { readonly [key: string]: string | undefined }): void {
   jest.spyOn(envVar, 'get').mockImplementation((...args: readonly any[]) => {
     const originalEnvVar = jest.requireActual('env-var');
     const env = originalEnvVar.from(envVars);
 
     return env.get(...args);
   });
+}
+
+export async function generateStubNodeCertificate(
+  publicKey: CryptoKey,
+  privateKey: CryptoKey,
+): Promise<Certificate> {
+  const tomorrow = new Date();
+  tomorrow.setDate(tomorrow.getDate() + 1);
+
+  return issueNodeCertificate({
+    issuerPrivateKey: privateKey,
+    serialNumber: 1,
+    subjectPublicKey: publicKey,
+    validityEndDate: tomorrow,
+  });
+}
+
+export function expectBuffersToEqual(
+  buffer1: Buffer | ArrayBuffer,
+  buffer2: Buffer | ArrayBuffer,
+): void {
+  if (buffer1 instanceof Buffer) {
+    expect(buffer2).toBeInstanceOf(Buffer);
+    expect(buffer1.equals(buffer2 as Buffer)).toBeTrue();
+  } else {
+    expect(buffer1).toBeInstanceOf(ArrayBuffer);
+    expect(buffer2).toBeInstanceOf(ArrayBuffer);
+
+    const actualBuffer1 = Buffer.from(buffer1);
+    const actualBuffer2 = Buffer.from(buffer2);
+    expect(actualBuffer1.equals(actualBuffer2)).toBeTrue();
+  }
 }
