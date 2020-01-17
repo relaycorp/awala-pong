d407a5766bc29dd421be7c581f39d682330e892b
Gus Narea <gnarea@users.noreply.github.com>
2020-01-17 13:29:53 +0000

    Refactor process before actual implementation of PR

--- src/app/background_queue/processor.ts
+++ src/app/background_queue/processor.ts
@@ -27,53 +27,63 @@ export default async function processPing(job: Job): Promise<void> {
 
   // We should be supporting multiple keys so we can do key rotation.
   // See: https://github.com/relaycorp/relaynet-pong/issues/14
+  const privateKey = await getEndpointPrivateKey();
+
+  const ping = await unwrapPing(queueMessage.serviceMessageCiphertext, privateKey, job.id);
+  if (ping === undefined) {
+    // Service message was invalid; errors were already logged.
+    return;
+  }
+
+  const pongRecipientCertificate = Certificate.deserialize(
+    bufferToArray(base64ToDer(queueMessage.senderCertificate)),
+  );
+  const pongServiceMessage = await generatePongServiceMessage(ping, pongRecipientCertificate);
+  const pongParcel = new Parcel(
+    pongRecipientCertificate.getCommonName(),
+    ping.pda,
+    pongServiceMessage,
+  );
+  const parcelSerialized = await pongParcel.serialize(privateKey);
+  await deliverParcel(queueMessage.gatewayAddress, parcelSerialized);
+}
+
+async function getEndpointPrivateKey(): Promise<CryptoKey> {
   const privateKeyPem = getEnvVar('ENDPOINT_PRIVATE_KEY')
     .required()
     .asString();
-  const privateKey = await convertPemPrivateKeyToWebCrypto(privateKeyPem);
+  return convertPemPrivateKeyToWebCrypto(privateKeyPem);
+}
 
+async function unwrapPing(
+  serviceMessageCiphertext: string,
+  privateKey: CryptoKey,
+  jobId: string | number,
+): Promise<Ping | undefined> {
   // tslint:disable-next-line:no-let
   let serviceMessage;
   try {
-    serviceMessage = await extractServiceMessage(queueMessage.serviceMessageCiphertext, privateKey);
+    serviceMessage = await extractServiceMessage(serviceMessageCiphertext, privateKey);
   } catch (error) {
     // The sender didn't create a valid service message, so let's ignore it.
-    logger.info('Invalid service message', { err: error, jobId: job.id });
+    logger.info('Invalid service message', { err: error, jobId });
     return;
   }
 
   if (serviceMessage.type !== 'application/vnd.relaynet.ping-v1.ping') {
     logger.info('Invalid service message type', {
-      jobId: job.id,
+      jobId,
       messageType: serviceMessage.type,
     });
     return;
   }
 
-  // tslint:disable-next-line:no-let
-  let ping: Ping;
   try {
-    ping = deserializePing(serviceMessage.value);
+    return deserializePing(serviceMessage.value);
   } catch (error) {
-    logger.info('Invalid ping message', { err: error, jobId: job.id });
+    logger.info('Invalid ping message', { err: error, jobId });
     return;
   }
-
-  const pongMessage = new ServiceMessage('application/vnd.relaynet.ping-v1.pong', ping.id);
-  const pongRecipientCertificate = Certificate.deserialize(
-    bufferToArray(base64ToDer(queueMessage.senderCertificate)),
-  );
-  const pongServiceMessage = await SessionlessEnvelopedData.encrypt(
-    pongMessage.serialize(),
-    pongRecipientCertificate,
-  );
-  const pongParcel = new Parcel(
-    pongRecipientCertificate.getCommonName(),
-    ping.pda,
-    pongServiceMessage.serialize(),
-  );
-  const parcelSerialized = await pongParcel.serialize(privateKey);
-  await deliverParcel(queueMessage.gatewayAddress, parcelSerialized);
 }
 
 async function extractServiceMessage(
@@ -89,6 +99,18 @@ async function extractServiceMessage(
   ) as SessionlessEnvelopedData;
   const serviceMessageSerialized = await serviceMessageCiphertext.decrypt(privateKey);
   return ServiceMessage.deserialize(Buffer.from(serviceMessageSerialized));
+}
+
+async function generatePongServiceMessage(
+  ping: Ping,
+  recipientCertificate: Certificate,
+): Promise<ArrayBuffer> {
+  const pongMessage = new ServiceMessage('application/vnd.relaynet.ping-v1.pong', ping.id);
+  const pongServiceMessage = await SessionlessEnvelopedData.encrypt(
+    pongMessage.serialize(),
+    recipientCertificate,
+  );
+  return pongServiceMessage.serialize();
 }
 
 async function convertPemPrivateKeyToWebCrypto(privateKeyPem: string): Promise<CryptoKey> {
