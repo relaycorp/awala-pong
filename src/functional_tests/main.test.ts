512853e000d24998936a0b0a475283aa8d6ef261
Gus Narea <gnarea@users.noreply.github.com>
2020-01-22 15:36:45 +0000

    Complete implementation of sessionless e2e test

--- src/functional_tests/main.test.ts
+++ src/functional_tests/main.test.ts
@@ -1,55 +1,79 @@
-import { Certificate, generateRSAKeyPair } from '@relaycorp/relaynet-core';
+/* tslint:disable:no-let */
+import {
+  Certificate,
+  generateRSAKeyPair,
+  Parcel,
+  ServiceMessage,
+  SessionlessEnvelopedData,
+} from '@relaycorp/relaynet-core';
 import { deliverParcel } from '@relaycorp/relaynet-pohttp';
 import bufferToArray from 'buffer-to-arraybuffer';
 import * as fs from 'fs';
-import { logDiffOn501, Stubborn } from 'stubborn-ws';
+import { logDiffOn501, Route, Stubborn } from 'stubborn-ws';
 
 import { generateStubNodeCertificate, generateStubPingParcel } from '../app/_test_utils';
 
+const GATEWAY_PORT = 4000;
+const GATEWAY_ADDRESS = `http://gateway:${GATEWAY_PORT}/`;
 const PONG_SERVICE_ENDPOINT = 'http://app:3000/';
 const ENDPOINT_CERTIFICATE_DER = fs.readFileSync(__dirname + '/endpoint-certificate.der');
 
 describe('End-to-end test for successful delivery of ping and pong messages', () => {
-  //region Configure Stubborn
   const mockGatewayServer = new Stubborn({ host: '0.0.0.0' });
-  beforeAll(async () => mockGatewayServer.start(4000));
+  beforeAll(async () => mockGatewayServer.start(GATEWAY_PORT));
   afterAll(async () => mockGatewayServer.stop());
   afterEach(() => mockGatewayServer.clear());
-  //endregion
 
-  test('Gateway should receive pong message', async () => {
-    const gatewayEndpointRoute = mockGatewayServer
+  let gatewayEndpointRoute: Route;
+  beforeEach(() => {
+    gatewayEndpointRoute = mockGatewayServer
       .post('/')
       .setHeader('Content-Type', 'application/vnd.relaynet.parcel')
       .setBody(body => !!body)
       .setResponseStatusCode(202);
     logDiffOn501(mockGatewayServer, gatewayEndpointRoute);
+  });
 
-    const endpointCertificate = Certificate.deserialize(bufferToArray(ENDPOINT_CERTIFICATE_DER));
+  let endpointCertificate: Certificate;
+  let pingSenderPrivateKey: CryptoKey;
+  let pingSenderCertificate: Certificate;
+  beforeAll(async () => {
+    endpointCertificate = Certificate.deserialize(bufferToArray(ENDPOINT_CERTIFICATE_DER));
+
     const pingSenderKeyPair = await generateRSAKeyPair();
-    const pingSenderCertificate = await generateStubNodeCertificate(
+    pingSenderPrivateKey = pingSenderKeyPair.privateKey;
+    pingSenderCertificate = await generateStubNodeCertificate(
       pingSenderKeyPair.publicKey,
-      pingSenderKeyPair.privateKey,
+      pingSenderPrivateKey,
     );
+  });
+
+  test('Gateway should receive pong message', async () => {
     const pingParcel = bufferToArray(
       await generateStubPingParcel(PONG_SERVICE_ENDPOINT, endpointCertificate, {
         certificate: pingSenderCertificate,
-        privateKey: pingSenderKeyPair.privateKey,
+        privateKey: pingSenderPrivateKey,
       }),
     );
     await deliverParcel(PONG_SERVICE_ENDPOINT, pingParcel, {
-      relayAddress: `http://gateway:${mockGatewayServer.getPort()}/`,
+      relayAddress: GATEWAY_ADDRESS,
     });
 
-    await sleep(4000);
-
+    await sleep(2000);
     expect(gatewayEndpointRoute.countCalls()).toEqual(1);
 
-    // const body = { some: 'body' };
-    //
-    // const res = await axios.get(`${mockGatewayServer.getOrigin()}`);
-    //
-    // expect(res.data).toEqual(body);
+    const pongParcelSerialized = (gatewayEndpointRoute.getCall(0).body as unknown) as Buffer;
+    const pongParcel = await Parcel.deserialize(bufferToArray(pongParcelSerialized));
+    expect(pongParcel).toHaveProperty('recipientAddress', pingSenderCertificate.getCommonName());
+    const pongParcelPayload = SessionlessEnvelopedData.deserialize(
+      bufferToArray(pongParcel.payloadSerialized as Buffer),
+    );
+    const pongServiceMessageSerialized = await pongParcelPayload.decrypt(pingSenderPrivateKey);
+    const pongServiceMessage = ServiceMessage.deserialize(
+      Buffer.from(pongServiceMessageSerialized),
+    );
+    expect(pongServiceMessage).toHaveProperty('type', 'application/vnd.relaynet.ping-v1.pong');
+    expect(pongServiceMessage).toHaveProperty('value.byteLength', 36);
   });
 
   test.todo('Channel session protocol');
