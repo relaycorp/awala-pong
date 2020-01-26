7ede361f625123289513ebdb00a3933cb52c32f3
Gus Narea <gnarea@users.noreply.github.com>
2020-01-26 17:53:09 +0000

    feat: Generate initial session key pair along with long-term endpoint key pair

--- README.md
+++ README.md
@@ -31,12 +31,12 @@ Requirements: Docker, docker-compose.
 docker-compose up --build --remove-orphan
 ```
 
-When running for the first time, make sure to generate the endpoint keys in Vault:
+When running for the first time, make sure to generate the endpoint and session keys in Vault:
 
 ```
 docker-compose exec -e VAULT_ADDR='http://127.0.0.1:8200' -e VAULT_TOKEN=letmein vault vault secrets enable -path=pong-keys kv-v2
 
-docker-compose run --rm queue src/bin/generate-endpoint-keypair.ts
+docker-compose run --rm queue src/bin/generate-keypairs.ts
 ```
 
 Then go to http://127.0.0.1:8080/
---
+++ src/bin/generate-keypairs.ts
@@ -0,0 +1,76 @@
+// tslint:disable:no-console
+
+// tslint:disable-next-line:no-var-requires no-console
+require('make-promises-safe');
+
+import { base64Encode } from '../app/utils';
+
+import {
+  generateECDHKeyPair,
+  generateRSAKeyPair,
+  issueInitialDHKeyCertificate,
+  issueNodeCertificate,
+} from '@relaycorp/relaynet-core';
+import { get as getEnvVar } from 'env-var';
+
+import { VaultPrivateKeyStore } from '../app/vaultPrivateKeyStore';
+
+const NODE_CERTIFICATE_TTL_DAYS = 180;
+const SESSION_CERTIFICATE_TTL_DAYS = 60;
+
+const PONG_ENDPOINT_KEY_ID = getEnvVar('ENDPOINT_KEY_ID')
+  .required()
+  .asString();
+const vaultUrl = getEnvVar('VAULT_URL')
+  .required()
+  .asString();
+const vaultToken = getEnvVar('VAULT_TOKEN')
+  .required()
+  .asString();
+const vaultKvPrefix = getEnvVar('VAULT_KV_PREFIX')
+  .required()
+  .asString();
+const sessionStore = new VaultPrivateKeyStore(vaultUrl, vaultToken, vaultKvPrefix);
+
+async function main(): Promise<void> {
+  try {
+    await sessionStore.fetchNodeKey(PONG_ENDPOINT_KEY_ID);
+    console.warn(`Endpoint key ${PONG_ENDPOINT_KEY_ID} already exists`);
+    return;
+  } catch (error) {
+    console.log(`Endpoint key will be created because it doesn't already exist`);
+  }
+
+  const endpointKeyPair = await generateRSAKeyPair();
+
+  const nodeCertendDate = new Date();
+  nodeCertendDate.setDate(nodeCertendDate.getDate() + NODE_CERTIFICATE_TTL_DAYS);
+  const endpointCertificate = await issueNodeCertificate({
+    isCA: true,
+    issuerPrivateKey: endpointKeyPair.privateKey,
+    subjectPublicKey: endpointKeyPair.publicKey,
+    validityEndDate: nodeCertendDate,
+  });
+
+  await sessionStore.saveNodeKey(endpointKeyPair.privateKey, PONG_ENDPOINT_KEY_ID);
+
+  const initialSessionKeyPair = await generateECDHKeyPair();
+  const sessionCertEndDate = new Date();
+  sessionCertEndDate.setDate(sessionCertEndDate.getDate() + SESSION_CERTIFICATE_TTL_DAYS);
+  const initialKeyCertificate = await issueInitialDHKeyCertificate({
+    dhPublicKey: initialSessionKeyPair.publicKey,
+    nodeCertificate: endpointCertificate,
+    nodePrivateKey: endpointKeyPair.privateKey,
+    validityEndDate: sessionCertEndDate,
+  });
+
+  console.log(
+    JSON.stringify({
+      endpointCertificate: base64Encode(endpointCertificate.serialize()),
+      initialSessionCertificate: base64Encode(initialKeyCertificate.serialize()),
+      keyPairId: PONG_ENDPOINT_KEY_ID,
+    }),
+  );
+}
+
+main();
