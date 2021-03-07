88d251bceae4041b51ac3520c8c0abcb2c75f927
Gus Narea <gnarea@users.noreply.github.com>
2021-03-07 11:23:54 +0000

    fix broken test, 2

--- src/functional_tests/main.test.ts
+++ src/functional_tests/main.test.ts
@@ -164,7 +164,7 @@ describe('End-to-end test for successful delivery of ping and pong messages', ()
     const pongServiceMessageSerialized = await pongParcelPayload.decrypt(recipientPrivateKey);
     const pongServiceMessage = ServiceMessage.deserialize(pongServiceMessageSerialized);
     expect(pongServiceMessage).toHaveProperty('type', 'application/vnd.relaynet.ping-v1.pong');
-    expect(pongServiceMessage).toHaveProperty('value.byteLength', 36);
+    expect(pongServiceMessage).toHaveProperty('content.byteLength', 36);
   }
 
   function configureMockGatewayServer(): void {
