f526a49febfe60c8b50cff1c7e9293a19edc3e18
Gus Narea <gnarea@users.noreply.github.com>
2022-04-23 11:35:53 +0200

    actually fix port forwarding

--- .github/scripts/port-forward.sh
+++ .github/scripts/port-forward.sh
@@ -7,7 +7,7 @@ set -x
 # Ugly workaround for https://github.com/GoogleContainerTools/skaffold/issues/4022
 
 kubectl port-forward --address 127.0.0.1 svc/relaynet-pong-pohttp 8080:80 &
-kubectl port-forward --address 127.0.0.1 deploy/mockserver 1080:80 &
+kubectl port-forward --address 127.0.0.1 svc/mock-public-gateway 1080:80 &
 
 # Check at least one of the ports:
-timeout 5 sh -c "while ! wget --spider http://127.0.0.1:8080 ; do sleep 1s ; done"
+timeout 5 sh -c "while ! wget --spider http://127.0.0.1:1080 ; do sleep 1s ; done"
