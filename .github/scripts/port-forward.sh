aad30a1e133f62c3297f10d60fbaada2579e8ebe
Gus Narea <gnarea@users.noreply.github.com>
2022-04-23 11:40:26 +0200

    make port forwading more reliable

--- .github/scripts/port-forward.sh
+++ .github/scripts/port-forward.sh
@@ -6,8 +6,13 @@ set -x
 
 # Ugly workaround for https://github.com/GoogleContainerTools/skaffold/issues/4022
 
-kubectl port-forward --address 127.0.0.1 svc/relaynet-pong-pohttp 8080:80 &
-kubectl port-forward --address 127.0.0.1 svc/mock-public-gateway 1080:80 &
+function port_forward() {
+  local service="$1"
+  local local_port="$2"
 
-# Check at least one of the ports:
-timeout 5 sh -c "while ! wget --spider http://127.0.0.1:1080 ; do sleep 1s ; done"
+  kubectl port-forward --address 127.0.0.1 "svc/${service}" "${local_port}:80" &
+  timeout 5 sh -c "while ! wget --spider http://127.0.0.1:${local_port} ; do sleep 1s ; done"
+}
+
+port_forward relaynet-pong-pohttp 8080
+port_forward mock-public-gateway 1080
