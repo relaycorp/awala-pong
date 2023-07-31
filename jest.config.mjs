9cd462bf9ada6d7b8961945dc697054976c8ffc8
Gus Narea <gnarea@users.noreply.github.com>
2023-07-31 14:43:12 +0100

    simplify jest config

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,14 +1,12 @@
 import baseConfig from '@relaycorp/shared-config/jest.mjs';
+
 export default {
   ...baseConfig,
-  setupFilesAfterEnv: [...baseConfig.setupFilesAfterEnv ?? [], 'jest-extended/all'],
+  setupFilesAfterEnv: ['jest-extended/all'],
   coveragePathIgnorePatterns: [
-    "_test_utils\.ts",
     "/bin",
     "/functionalTests",
     "/testUtils",
-    "/types",
-    "/index\.ts",
   ],
   testPathIgnorePatterns: [
     ...baseConfig.testPathIgnorePatterns,
