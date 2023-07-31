96dc747c356a98830a0249586410cc9bc50d8e29
Gus Narea <gnarea@users.noreply.github.com>
2023-07-31 14:50:15 +0100

    prettyfy everything

--- jest.config.mjs
+++ jest.config.mjs
@@ -3,13 +3,6 @@ import baseConfig from '@relaycorp/shared-config/jest.mjs';
 export default {
   ...baseConfig,
   setupFilesAfterEnv: ['jest-extended/all'],
-  coveragePathIgnorePatterns: [
-    "/bin",
-    "/functionalTests",
-    "/testUtils",
-  ],
-  testPathIgnorePatterns: [
-    ...baseConfig.testPathIgnorePatterns,
-    "/src/functionalTests/",
-  ],
+  coveragePathIgnorePatterns: ['/bin', '/functionalTests', '/testUtils'],
+  testPathIgnorePatterns: [...baseConfig.testPathIgnorePatterns, '/src/functionalTests/'],
 };
